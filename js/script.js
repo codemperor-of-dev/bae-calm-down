5c9fb0cbefd028816c36a43c23f87d7c4b10fe29
OFFICIALCALEB <caleb86141@gmail.com>
2022-10-24 16:51:47 +0630

    late-bae

--- js/script.js
+++ js/script.js
@@ -14,47 +14,92 @@ switchBtn.forEach((button) => {
   });
 });
 
-$(".single-item").slick();
-var options = {
+let options = {
   strings: ["Please...", "Fill Form..."],
   typeSpeed: 50,
   backSpeed: 40,
   backDelay: 2000,
   loop: true,
 };
-var typed = new Typed(".element", options);
-category = ["buy me some food", "ask me out for movie date", "buy me a gift"];
-let subCategory = [
-  { name: "what do you wanna eat?", category_id: 0 },
-  { name: "what movie would you love?", category_id: 1 },
-  { name: "what do you want as a love gift?", category_id: 2 },
-];
-category.map(function (el, index) {
-  $("#c").append(
-    `<option class="py-2 fs-6 ps-2" value="${index}">${el}</option>`
-  );
+
+let typed = new Typed(".element", options);
+const madQuestion = document.getElementById("mad-question");
+const sulkingQuestion = document.getElementById("sulking-question");
+const choiceQuestion = document.getElementById("choice-question");
+const moodQuestion = document.getElementById("mood-question");
+const seriousQuestion = document.getElementById("serious-question");
+
+const madForm = document.getElementById("mad-form");
+const sulkingForm = document.getElementById("sulking-form");
+const choiceForm = document.getElementById("choice-form");
+const moodForm = document.getElementById("mood-form");
+const upsetForm = document.getElementById("upset-form");
+const seriousForm = document.getElementById("serious-form");
+const allForm = document.getElementById("all-form");
+const thanksText = document.getElementById("thanks-text")
+const typedCursor = document.querySelector(".typed-cursor");
+
+sulkingForm.style.display = "none";
+choiceForm.style.display = "none";
+moodForm.style.display = "none";
+upsetForm.style.display = "none";
+seriousForm.style.display = "none";
+
+
+madQuestion.addEventListener("click", function () {
+  madForm.style.display = "none"
+  switch (this.value) {
+    case "1":
+      sulkingForm.style.display = "grid";
+ 
+      break;
+    case "2":
+      moodForm.style.display = "grid"
+      break;
+    case "3":
+      seriousForm.style.display = "grid"
+      break;
+  }
+});
+moodQuestion.addEventListener("click", function () {
+  if (this.value === "1") {
+    moodForm.style.display = "none";
+    upsetForm.style.display = "grid"
+  }
+});
+seriousQuestion.addEventListener("click", function () {
+  if (this.value === "1") {
+    seriousForm.style.display = "none"
+    madForm.style.display = "grid";
+  } else {
+    allForm.remove();
+    thanksText.remove();
+    typedCursor.remove();
+    document.body.innerHTML += `
+    <div  class="d-flex  justify-content-center p-5 m-5"">
+    <button class="submit-btn ">Submit</button>
+</div>
+<div  class="d-flex justify-content-center ">
+  <h1 class="text-color fw-bold">Thanks For Using Our Service</h1>
+</div>
+    `;
+  }
 });
 
-subCategory.map(function (el, index) {
-  $("#sc").append(
-    `<option value="${index}" class="p-5  fw-bold text-black text-center" data-category="${el.category_id}">${el.name}</option>`
-  );
-});
-
-$("#c").on("change", function () {
-  let currentCategoryId = $(this).val();
-  $("#sc option").hide();
-  $(`[data-category=${currentCategoryId}]`).show();
-});
-
-$("#last-next").on("change", function () {
-  $(".element").hide();
-  $(".bird-img").addClass("bird-position-submit");
-  $(".typed-cursor").hide();
-  $(".slick-arrow").hide();
-  $("#last-next").hide();
-  $("#submit-btn")
-    .removeClass("d-none")
-    .addClass("d-block animate__animated animate__pulse infinite");
-  $("#thanks-text").removeClass("d-none").addClass("d-block");
+sulkingQuestion.addEventListener("click", function () {
+console.log(this.value)
+sulkingForm.style.display = "none"
+choiceForm.style.display = "block"
+  switch (this.value) {
+    case "1":
+         
+        choiceQuestion.innerHTML = `<option class="color-form-bg sc-padding m-0 fw-bold text-white text-center"  disabled>What do you wanna eat?</option>`;
+      break;
+    case "2":  
+        choiceQuestion.innerHTML = `<option class="color-form-bg sc-padding m-0 fw-bold text-white text-center"  disabled>What movie would you love?</option>`;
+      break;
+    case "3": 
+        choiceQuestion.innerHTML = `<option class="color-form-bg sc-padding m-0 fw-bold text-white text-center"  disabled>What do you want as a love gift?</option>`;
+      break;
+  }
 });
